70d427ad904715d8d09a5c06a87dc357468b9fb7
Emirhan Zınkal <emirhanzinkal24@istanbularel.edu.tr>
2025-12-15 16:07:53 +0300

    NotificationsControllers Added

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e6b55cd820933b7037571a1031cc2038df065c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b4ea2fe85f2bab5234773de025e5dc0f376148")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
